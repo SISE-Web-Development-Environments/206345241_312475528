b590396b517b65ddbb054528a0da608001916fad
adiashk <ashkenazi94>
2020-04-18 21:39:52 +0300

    vaild-form

--- registration.js
+++ registration.js
@@ -1,71 +1,84 @@
-/* $.validator.setDefaults({
-	submitHandler: function() {
-		alert("submitted!");
-	}
-}); */
-$(document).ready(function() {
+
+
+$(document).ready(function () {
+
+	$.validator.addMethod("checkPsw", function(value) {
+		let letter = /[a-zA-Z]/; 
+		let number = /\d/; 
+		return (value.match(letter) && value.match(number)); 
+		
+	 });
+
+	 $.validator.addMethod("checkName", function(value) {
+		let letter = /[a-zA-Z]/; 
+		let number = /\d/; 
+		return (value.match(letter) && !value.match(number)); 
+	 }); 
 
 	// validate signup form on keyup and submit
-	$("#signupForm").validate({
+	$("form[name='signupForm']").validate({
 		rules: {
 			username: {
 				required: true
 			},
 			firstname: {
 				required: true,
-				regexp: "^[a-zA-Z]*$"
+				checkName: true
+				
 			},
 			lastname: {
 				required: true,
-				regexp: "^[a-zA-Z]*$"
+				checkName: true
 			},
 			password: {
 				required: true,
 				minlength: 6,
-				regexp: "^[a-zA-Z0-9]*$"
+ 				checkPsw: true
 			},
 			email: {
 				required: true,
 				email: true
 			},
 			date: {
-				required: true
-			},
+				required: true,
+				date: true
+			}
+		},
 			messages: {
-				username: "Please enter a username",
-				firstname: {
-					required: "This field cannot be empty",
-					regexp: "first name must contains only letters"
-				},
-				lastname: {
-					required: "This field cannot be empty",
-					regexp: "last name must contains only letters"
-				},
-				password: {
-					required: "This field cannot be empty",
-					minlength: "password must be at least 6 characters",
-					regexp: "password must contain only numbers and letters"
-				},
-				email: {
-					required: "This field cannot be empty",
-					email: "Please enter a valid email address"
-				},
-				date: {
-					required: "This field cannot be empty"
-				}			}
+			username: "Please enter a username",
+			firstname: {
+				required: "This field cannot be empty",
+				checkName: "This field cannot be with numbers"
+			},
+			lastname: {
+				required: "This field cannot be empty",
+				checkName: "This field cannot be with numbers"
+			},
+			password: {
+				required: "This field cannot be empty",
+				minlength: "password must be at least 6 characters",
+				checkPsw: "This field have to be with numbers and letters"
+			},
+			email: {
+				required: "This field cannot be empty",
+				email: "Please enter a valid email address"
+			},
+			date: {
+				required: "This field cannot be empty"
+			}
+		},
+		submitHandler: function (form) {
+			form.submit();
 		}
 	});
 });
 
 
 
-
-
-
 function swapDiv(newDiv, oldDiv) {
-/* 
-	$(newDiv).hide();
-	$(oldDiv).show(); */
+	/* 
+		$(newDiv).hide();
+		$(oldDiv).show(); */
 	document.getElementById(newDiv).style.display = "block";
 	document.getElementById(oldDiv).style.display = "none";
 }
